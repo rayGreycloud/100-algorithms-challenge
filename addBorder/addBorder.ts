26eefb820767da0e8c61f2fa54a4a354e5706b73
rayGreycloud <rayGreycloud@gmail.com>
2018-07-07 14:40:01 -0700

    Solution for addBorder to matrix of characters

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,19 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(picture: string[]): any {
+  const borderLength = picture[0].length + 2;
+  let wall = '';
 
+  for (let i = 0; i < borderLength; i++) {
+    wall = wall.concat('*');
+  }
+
+  for (let j = 0; j < picture.length; j++) {
+    picture[j] = `*${picture[j]}*`;
+  }
+
+  picture.unshift(wall);
+  picture.push(wall);
+
+  return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(['abc', 'ded']));
