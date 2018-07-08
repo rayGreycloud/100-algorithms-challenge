56329aecc7e922faf61cc0120f675a3a7c8bff47
rayGreycloud <rayGreycloud@gmail.com>
2018-07-08 11:19:04 -0700

    Add border of * around string matrix

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,10 +1,12 @@
 function addBorder(picture: string[]): any {
-  const borderLength = picture[0].length + 2;
-  let wall = '';
+  const wall: string = '*'.repeat(picture[0].length + 2);
 
-  for (let i = 0; i < borderLength; i++) {
-    wall = wall.concat('*');
-  }
+  // const borderLength = picture[0].length + 2;
+  // let wall = '';
+
+  // for (let i = 0; i < borderLength; i++) {
+  //   wall = wall.concat('*');
+  // }
 
   for (let j = 0; j < picture.length; j++) {
     picture[j] = `*${picture[j]}*`;
