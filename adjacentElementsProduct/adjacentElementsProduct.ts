1223a0eaead48220d3014c51ea2f61549efb2e8c
rayGreycloud <rayGreycloud@gmail.com>
2018-07-09 15:17:09 -0700

    Return largest product of 2 adjacent elements in array

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,3 +1,16 @@
-function adjacentElementsProduct(inputArray: number[]): number {}
+function adjacentElementsProduct(inputArray: number[]): number {
+  // Initial value to first product
+  let largestProduct = inputArray[0] * inputArray[1];
+
+  // Iterate thru array - skip last item because nothing to its right
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    // Multiply number by next number in array
+    const product = inputArray[i] * inputArray[i + 1];
+    // Update result if larger than previous
+    largestProduct = largestProduct < product ? product : largestProduct;
+  }
+  // Return result
+  return largestProduct;
+}
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
