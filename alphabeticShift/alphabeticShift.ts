4cd259e345d4ca0a3003177f1021e76ce5105eef
rayGreycloud <rayGreycloud@gmail.com>
2018-07-14 16:24:14 -0700

    Shift letters in string by one character

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,3 +1,13 @@
-function alphabeticShift(inputString: string): string {}
+function alphabeticShift(inputString: string): string {
+  let charCodes = [];
+
+  for (let i = 0; i < inputString.length; i++) {
+    const charCode = inputString.charCodeAt(i);
+
+    charCodes.push(charCode === 122 ? 97 : charCode + 1);
+  }
+
+  return String.fromCharCode(...charCodes);
+}
 
 console.log(alphabeticShift('crazy'));
