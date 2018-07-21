7adf328f2d42aaa97c75cba239e1367829943cd7
rayGreycloud <rayGreycloud@gmail.com>
2018-07-21 15:51:50 -0700

    Return sums of alternate numbers

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,3 +1,16 @@
-function alternatingSums(a: number[]): number[] {}
+function alternatingSums(a: number[]): number[] {
+  let teamOneSum = 0;
+  let teamTwoSum = 0;
+
+  a.map((weight: number, index: number) => {
+    if (index % 2 !== 0) {
+      teamTwoSum += weight;
+    } else {
+      teamOneSum += weight;
+    }
+  });
+
+  return [teamOneSum, teamTwoSum];
+}
 
 console.log(alternatingSums([50, 60, 60, 45, 70]));
