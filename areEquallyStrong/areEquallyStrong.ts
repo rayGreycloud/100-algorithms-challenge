3d502d862a2e4163df44d77218493699da71a67c
rayGreycloud <rayGreycloud@gmail.com>
2018-07-23 19:49:35 -0700

    Sort and compare to arrays

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,20 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest: number = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest: number = yourLeft <= yourRight ? yourRight : yourLeft;
 
+  const friendsWeakest: number =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest: number =
+    friendsLeft <= friendsRight ? friendsRight : friendsLeft;
+
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 10));
+console.log(areEquallyStrong(15, 10, 15, 9));
