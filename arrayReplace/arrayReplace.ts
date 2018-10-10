6eed982ffdfedc8e8bda743359485607a5965da7
rayGreycloud <rayGreycloud@gmail.com>
2018-10-09 19:52:59 -0700

    array replace problem

--- arrayReplace/arrayReplace.ts
+++ arrayReplace/arrayReplace.ts
@@ -2,6 +2,14 @@ function arrayReplace(
   inputArray: number[],
   elemToReplace: number,
   substitutionElem: number
-): number[] {}
+): number[] {
+  inputArray.forEach((element, index) => {
+    if (element === elemToReplace) {
+      inputArray[index] = substitutionElem;
+    }
+  });
+
+  return inputArray;
+}
 
 console.log(arrayReplace([1, 2, 1], 1, 3));
